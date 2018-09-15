51e372c695215eb1c606fd3cb08b93cf07019294
edieyoung <370676740@qq.com>
2018-09-15 17:21:26 -0500

    json

--- python/vgg-face.py
+++ python/vgg-face.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 from keras.models import Model, Sequential
 from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
@@ -12,68 +10,9 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
-
-
-# In[2]:
-
-model = Sequential()
-model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-model.add(Convolution2D(4096, (7, 7), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(4096, (1, 1), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(2622, (1, 1)))
-model.add(Flatten())
-model.add(Activation('softmax'))
-
-
-# In[3]:
-
-
-#you can download the pretrained weights from the following link 
-#https://drive.google.com/file/d/1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo/view?usp=sharing
-#or you can find the detailed documentation https://sefiks.com/2018/08/06/deep-face-recognition-with-keras/
-
+import json
 from keras.models import model_from_json
-model.load_weights('../model/vgg_face_weights.h5')
 
-
-# In[4]:
 
 def preprocess_image(image_path):
     img = load_img(image_path, target_size=(224, 224))
@@ -81,9 +20,6 @@ def preprocess_image(image_path):
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
     return img
-
-
-# In[5]:
 
 
 def findCosineSimilarity(source_representation, test_representation):
@@ -98,19 +34,15 @@ def findEuclideanDistance(source_representation, test_representation):
     euclidean_distance = np.sqrt(euclidean_distance)
     return euclidean_distance
 
-
-vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
-
-
-def verifyFace(img1, img2):
-    img1_representation = vgg_face_descriptor.predict(preprocess_image('../dataset/victims/%s' % (img1)))[0,:]
-    img2_representation = vgg_face_descriptor.predict(preprocess_image('../dataset/testing/%s' % (img2)))[0,:]
+def verifyFace(img1, img2,vgg_face_descriptor):
+    img1_representation = vgg_face_descriptor.predict(preprocess_image('../dataset/testing/%s' % (img1)))[0,:]
+    img2_representation = vgg_face_descriptor.predict(preprocess_image('../dataset/victims/%s' % (img2)))[0,:]
     cosine_similarity = findCosineSimilarity(img1_representation, img2_representation)
-    euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
+    #euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
     epsilon1 = 0.30
     epsilon2 = 120
     cosineSame = epsilon1 - cosine_similarity
-    euclideanSame = epsilon2- euclidean_distance
+    #euclideanSame = epsilon2- euclidean_distance
     # print("Cosine similarity: ",cosine_similarity)
     # print("Euclidean distance: ",euclidean_distance)
     # print("similarity:",similarity)
@@ -131,18 +63,71 @@ def verifyFace(img1, img2):
     return cosineSame
 #img1 is the tested image path, and dataset needs to be json.
 def matchFaces(img1, dataset):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+
+#you can download the pretrained weights from the following link 
+#https://drive.google.com/file/d/1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo/view?usp=sharing
+#or you can find the detailed documentation https://sefiks.com/2018/08/06/deep-face-recognition-with-keras/
+    model.load_weights('../model/vgg_face_weights.h5')
+    vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
     top10 = {}
     top10[0] = 100000000
     for item in dataset:
-        img2 = getImagePath(data[item]["path"])
-        similarity = verifyFace(img1, img2)
+        img2 = dataset[item]["path"]
+        victimID = dataset[item]["id"]
+        similarity = verifyFace(img1, img2,vgg_face_descriptor)
         if similarity>0:
             if len(top10)<11:
-                top10[id] = similarity
+                top10[victimID] = similarity
             else:
                 del top10[findMin(top10)]
-                top10[id] = similarity
+                top10[victimID] = similarity
     del top10[0]
+    for key in top10.keys():
+        print(key)
     return top10.keys()
 
 def findMin(l):
@@ -152,7 +137,18 @@ def findMin(l):
             mini = i
     return mini
 
-                
+def main():
+    img = "2.jpg"
+    victims_file = open('./victims')
+    json1_str = victims_file.read()
+    dataset = json.loads(json1_str)
+    faceList = matchFaces(img, dataset)
+    print(faceList)
+
+if __name__=="__main__":
+    main()
+
+
 
 
 
